c07c03792d297f1c949a952515b437c692523be0
divya-sahu08 <divyasahu14591@gmail.com>
2025-04-06 21:28:48 +0530

    percentage and absolute value

--- 5PieChart.py
+++ 5PieChart.py
@@ -6,9 +6,17 @@ import matplotlib.pyplot as plt
 # plt.pie(x,labels=y)
 # plt.show()
 
-x=[20,30,10]
-y=["java","cpp","python"]
-ex=[0.1,0.0,0.0]
-c=["r","b","g"]
-plt.pie(x,labels=y,explode=ex,colors=c,shadow="True",radius=0.3,textprops={"fontsize":15},counterclock=False)
+# x=[20,30,10]
+# y=["java","cpp","python"]
+# ex=[0.1,0.0,0.0]
+# c=["r","b","g"]
+# plt.pie(x,labels=y,explode=ex,colors=c,shadow="True",radius=0.3,textprops={"fontsize":15},counterclock=False)
+# plt.show()
+
+
+x=["india","china","russia","germany","poland"]
+y=[12,32,15,20,22]
+
+c=["red","green","blue","purple","white"]
+plt.pie(y,labels=x,colors=c,autopct="%.0f", wedgeprops={'edgecolor': 'black'}) #autopct="%,2f"(for displaying in the form of percentage)
 plt.show()
